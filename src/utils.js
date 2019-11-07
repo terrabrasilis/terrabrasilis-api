caaa64df4c34415fc18a80a8cf0358b7a12f0e4e
Paulo Luan <1131963+PauloLuan@users.noreply.github.com>
2019-11-07 10:45:06 -0300

    fixed missing question mark

--- src/utils.js
+++ src/utils.js
@@ -66,7 +66,7 @@ const Utils = {
 
   configureUrlWorkspace: (layerConfig) => {
     let baseUrl = layerConfig.datasource.host.replace('ows', layerConfig.workspace + '/' + layerConfig.name + '/ows')
-    baseUrl += `REQUEST=GetCapabilities&VERSION=1.3.0&SERVICE=wms` 
+    baseUrl += `?REQUEST=GetCapabilities&VERSION=1.3.0&SERVICE=wms` 
     return baseUrl
   },
 
