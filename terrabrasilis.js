1f453f30d49fee057f99fcb1671619ae96f73c8a
Claudio Bogossian <claudio.bogossian@gmail.com>
2024-03-01 14:31:42 -0300

    Changing time dimension plugin to use singletiled layers.

--- terrabrasilis.js
+++ terrabrasilis.js
@@ -2442,6 +2442,150 @@ Terrabrasilis = (function () {
    }
 
 
+   /*
+ * L.SingleTile uses L.ImageOverlay to display a single-tile WMS layer.
+ * url parameter must accept WMS-style width, height and bbox.
+ */
+
+L.SingleTile = L.ImageOverlay.extend({
+	defaultWmsParams: {
+		service: 'WMS',
+		request: 'GetMap',
+		version: '1.1.1',
+		layers: '',
+		styles: '',
+		format: 'image/png',
+		transparent: true
+	},
+  _visible: true,
+  _loaded: false,
+
+  // _originalUpdate: L.NonTiledLayer.prototype._update,
+  // _originalOnRemove: L.NonTiledLayer.prototype.onRemove,
+
+	initialize: function( url, options) 
+  {
+		this.wmsParams = L.extend({      
+    }, this.defaultWmsParams);
+    this.wmsParams.layers=options.wmsParams.layers;
+    this.wmsParams.format=options.wmsParams.format;
+    this.wmsParams.time=options.wmsParams.time;
+
+    //console.log("Initializating layer: " + options.wmsParams.time);
+		L.ImageOverlay.prototype.initialize.call(this, url, null, options);
+	},
+
+	setParams: function (params) {
+		L.extend(this.wmsParams, params);
+		return this;
+	},
+
+	redraw: function () {
+		this._updateImageUrl();
+	},
+
+	onAdd: function (map) 
+  {
+    this._map = map;
+		var projectionKey = parseFloat(this.wmsParams.version) >= 1.3 ? 'crs' : 'srs';
+//		this.wmsParams[projectionKey] = 'EPSG:4326'; // this is incorrect!
+		this.wmsParams[projectionKey] = map.options.crs.code;
+		L.ImageOverlay.prototype.onAdd.call(this, map);
+    
+		map.on('moveend', this._updateImageUrl, this);
+	},
+
+	onRemove: function (map) {
+		map.on('moveend', this._updateImageUrl, this);
+		L.ImageOverlay.prototype.onRemove.call(this, map);
+	},
+
+	// Copypasted from L.ImageOverlay (dirty hack)
+	_initImage: function () {
+		this._image = L.DomUtil.create('img', 'leaflet-image-layer');
+
+		if (this._map.options.zoomAnimation && L.Browser.any3d) {
+			L.DomUtil.addClass(this._image, 'leaflet-zoom-animated');
+		} else {
+			L.DomUtil.addClass(this._image, 'leaflet-zoom-hide');
+		}
+
+		this._updateOpacity();
+		this._bounds = this._map.getBounds();
+
+		//TODO createImage util method to remove duplication
+		L.extend(this._image, {
+			galleryimg: 'no',
+			onselectstart: L.Util.falseFn,
+			onmousemove: L.Util.falseFn,
+			onload: L.bind(this._onImageLoad, this),
+			src: this._constructUrl()
+		});
+	},
+
+	_onImageLoad: function () 
+  {
+    if(this._map)
+    {
+      this._bounds = this._map.getBounds();
+    
+      this._reset();      
+    } 
+    this.fire('load');
+	},
+
+	_updateImageUrl: function () 
+  {
+    if(this._map)
+    {
+      this._image.src = this._constructUrl();
+    }
+		
+	},
+
+	_constructUrl: function () 
+  {    
+		var size = this._map.getSize();
+		var b = this._map.getBounds();
+    var crs = this._map.options.crs;
+    var ne = crs.project(b.getNorthEast());
+    var sw = crs.project(b.getSouthWest());
+    var bbox = new L.LatLngBounds(new L.LatLng(sw.y, sw.x), new L.LatLng(ne.y, ne.x))
+    
+
+		return this._url + L.Util.getParamString(this.wmsParams, this._url) + "&width=" + size.x + "&height=" + size.y + "&bbox=" + bbox.toBBoxString();
+	},
+
+
+  setLoaded: function(loaded) {
+    this._loaded = loaded;
+},
+
+isLoaded: function() {
+    return this._loaded;
+},
+hide: function() {
+  this._visible = false;
+  if(this._div)
+  {
+    this._div.style.display = 'none';
+  }  
+},
+
+show: function() {
+  this._visible = true;
+  if(this._div)
+  {    
+    this._div.style.display = 'block';
+  }
+},
+});
+
+L.singleTile = function (url, options) {
+	return new L.SingleTile(url, options);
+};
+
+
 
   /// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // return
